ec6d412876b449b34d01a37529b56baacf9d7619
LeeDaeGyeong <dleorud1027@gmail.com>
2024-01-19 20:27:25 +0900

    feat >> task3-3 보류

--- task3/2week/src/question/components/one-todo.tsx
+++ task3/2week/src/question/components/one-todo.tsx
@@ -1,56 +1,80 @@
 import { TodoDataBase, TodoType, TodoEnum } from "@/types/todo";
-import { useState } from "react";
 
 type Props = {
   todo: TodoDataBase;
 };
 
-const OneTodo: React.FC<Props> = ({ todo }) => {
-  // const [currentTodo, setCurrentTodo] = useState()
-
-  // export type TodoType<T extends TodoEnum = TodoEnum> = Extract<
-  //   TodoDataBase,
-  //   { type: T }
-  // >;
+const OneTodo: React.FC<Props> = () => {
+  let todoData: any = {
+    type: TodoEnum.DAILY,
+    content: "aa",
+    title: "aa",
+  };
 
   // const isDailyTodo = (
   //   todo: TodoDataBase
   // ): todo is TodoType<TodoEnum.DAILY> => {
-  //   return todo.type === "DAILY";
+  //   return todo.type === TodoEnum.DAILY;
   // };
 
   // const isWeeklyTodo = (
   //   todo: TodoDataBase
   // ): todo is TodoType<TodoEnum.WEEKLY> => {
-  //   return todo.type === "WEEKLY";
+  //   return todo.type === TodoEnum.WEEKLY;
   // };
 
   // const isMonthlyTodo = (
   //   todo: TodoDataBase
   // ): todo is TodoType<TodoEnum.MONTHLY> => {
-  //   return todo.type === "MONTHLY";
+  //   return todo.type === TodoEnum.MONTHLY;
   // };
 
-  const todoType = (todo: TodoDataBase) => {
-    if (todo.type === TodoEnum.DAILY) {
-      return {
-        title: todo.title,
-        content: todo.content,
-      };
-    }
-    if (todo.type === TodoEnum.WEEKLY) {
-      return {
-        total: todo.total,
-      };
-    }
-    if (todo.type === TodoEnum.MONTHLY) {
-      return {
-        goal: todo.goal,
-      };
-    }
+  // //타입을 바꿔주는 함수
+  // isDailyTodo(todoData);
+  // isWeeklyTodo(todoData);
+  // isMonthlyTodo(todoData);
+
+  const isTodoType = <T extends TodoEnum>(
+    todo: TodoDataBase,
+    targetType: T
+  ): todo is TodoType<T> => {
+    return todo.type === targetType;
   };
 
-  return <div>{todoType(todo)?.goal}</div>;
+  Object.values(TodoEnum).forEach((key) => {
+    if (isTodoType<typeof key>(todoData, todoData.type)) {
+      return todoData;
+    }
+  });
+
+  //   export type TodoType<T extends TodoEnum = TodoEnum> = Extract<
+  //   TodoDataBase,
+  //   { type: T }
+  // >;
+
+  // const getTodo = (todo: TodoDataBase) => {
+  //   if (todo.type === TodoEnum.DAILY) {
+  //     return {
+  //       type: todo.type,
+  //       title: todo.title,
+  //       content: todo.content,
+  //     };
+  //   }
+  //   if (todo.type === TodoEnum.WEEKLY) {
+  //     return {
+  //       type: todo.type,
+  //       total: todo.total,
+  //     };
+  //   }
+  //   if (todo.type === TodoEnum.MONTHLY) {
+  //     return {
+  //       type: todo.type,
+  //       goal: todo.goal,
+  //     };
+  //   }
+  // };
+
+  return <>{/* <div>{getTodo(todo)?.goal}</div> */}</>;
 };
 
 export default OneTodo;
